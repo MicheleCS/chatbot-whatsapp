251fdebdcf0aae76927acdd72804d8e442e8d83a
MicheleCS <michelesouza-@outlook.com>
2023-06-21 10:45:30 -0300

    Configurações iniciais e estruturação da aplicação

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello World")
+import { Message, Whatsapp, create } from "venom-bot"
+
+create({
+  session: "chatbot-whatsapp",
+  disableWelcome: true,
+})
+  .then(async (client: Whatsapp) => await start(client))
+  .catch((err) => {
+    console.log(err)
+  })
+
+async function start(client: Whatsapp) {
+  client.onMessage(async (message: Message) => {
+    if (!message.body || message.isGroupMsg) return
+
+    const response = `Olá!`
+
+    await client.sendText(message.from, response)
+  })
+}
